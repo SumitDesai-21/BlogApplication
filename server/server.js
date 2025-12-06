66f5e97929be27cb4b67a651d4f0aa75858c45e8
SumitDesai-21 <sumit.desai3433@gmail.com>
2025-12-06 18:25:11 +0530

    Added Express & MongoDB

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-console.log("Sever is running on port 3000.");
+import express from 'express'
+import 'dotenv/config' // to use environment variables
+import cors from 'cors' // to connect backend with frontend
+
+// create express application
+const app = express();
+
+// built in middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+// home route
+app.get('/', (req, res)=>{
+    res.send("API is working");
+})
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, ()=>{
+    console.log("Server running on port: "+ PORT);
+})
+export default app;
