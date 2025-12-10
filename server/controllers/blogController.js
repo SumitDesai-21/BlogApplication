ce769f72f89d17d6ac154cb3afefffae6020a5d0
SumitDesai-21 <sumit.desai3433@gmail.com>
2025-12-10 20:37:22 +0530

    Added Controllers

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -40,9 +40,64 @@ const addBlog = async (req, res) =>{
         // we've all the data (blog data: title, subtitle, description, image etc)
         // now we'll store this data into MONGODB
 
+        // image will be uploaded in .webp format a modern file type to support all browsers.
+
         await Blog.create({title, subTitle, description, category, isPublished, image}); // await ur blog model
         res.json({success: true, message: "Blog Added Successfully"});
     }   
+    catch(error){
+        res.json({success: false, message: error.message});
+    }
+}
+
+export const getAllBlogs = async(req, res) =>{
+    try{
+        const blogs = await Blog.find({isPublished: true});
+        res.json({sucess: true, blogs});
+    }
+    catch(error){
+        res.json({sucess: false, message: error.message});
+    }
+}
+
+export const getBlogByID = async (req, res)=>{
+    try{
+        // get the blog id from parameter
+        const { blogId } = req.params;
+        const blog = await Blog.findById(blogId);
+        
+        if(!blogId){
+            return res.json({success:false, message: "Blog Not Found"});
+        }
+        res.json({success: true, blog});
+    }   
+    catch(error){
+        res.json({success: false, message: error.message});
+    }
+}
+
+// controller function to delete any blog
+export const deleteBlogByID = async (req, res)=>{
+    try{
+        // get the blog id from parameter
+        const { id } = req.body();
+        await Blog.findByIdAndDelete(id);
+        res.json({success: true, message: "Blog Deleted Successfully"});
+    }   
+    catch(error){
+        res.json({success: false, message: error.message});
+    }
+}
+
+// controller to publish or unpublish.
+export const togglePublish = async(req, res)=>{
+    try{
+        const {id} = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished = !blog.isPublished; // true to false & vice versa
+        await blog.save();
+        res.json({success: true, message: "Blog status updated"});
+    }
     catch(error){
         res.json({success: false, message: error.message});
     }
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,11 +2,17 @@ import express from 'express'
 
 import upload from '../middleware/multer.js';
 import auth from '../middleware/auth.js';
-import addBlog from '../controllers/blogController.js';
+import addBlog, { deleteBlogByID, getAllBlogs, getBlogByID, togglePublish } from '../controllers/blogController.js';
 
 // create express router
 const blogRouter = express.Router();
+// API endpoints
 blogRouter.post("/add", upload.single('image'), auth, addBlog);
+blogRouter.get('/all', getAllBlogs); // get all blogs 
+blogRouter.get('/:blogId', getBlogByID);    
+blogRouter.post('/delete',auth, deleteBlogByID);    
+blogRouter.post('/toggle-publish',auth, togglePublish);
+
 
 // add middleware
 // to parse the image we'll use the multer package.
