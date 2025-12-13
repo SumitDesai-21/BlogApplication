038dc245ab0c8f258f71b90c9f840321052eef09
SumitDesai-21 <sumit.desai3433@gmail.com>
2025-12-13 22:54:03 +0530

    Provided functionality to onSubmitHandler to Add Blog

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill';  
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const AddBlog = () => {
+
+  const { axios } = useAppContext();
+  const [isAdding, setIsAdding] = useState(false);
+  // when we'll upload blog it'll take some time to upload data to the database using API
+  // until then we'll we'll disble button & would show Adding...
+
+
   const editorRef = useRef(null);
-  const quillRef = useRef(null);
+  const quillRef = useRef(null); // rich text
   
   const [image, setImage] = useState(false);
   const [title, setTitle] = useState('');
@@ -12,8 +21,39 @@ const AddBlog = () => {
   const [category, setCategory] = useState('Startup');
   const [isPublished, setIsPublished] = useState(false);
   
+  // in these states we're getting form data
+  // we'll send this data on API & to make API call we need Axios
+
   const onSubmitHandler = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      setIsAdding(true) 
+
+      // create blog object
+      const blog = {
+        title, subTitle, 
+        description: quillRef.current.root.innerHTML,
+        category, isPublished
+      };
+
+      const formData = new FormData();
+      formData.append('blog', JSON.stringify(blog)); // stringfy blog data as it is an object
+      formData.append('image', image);
+
+      const { data } = await axios.post('/api/blog/add', formData);
+      if(data.success){
+          toast.success(data.message);
+          setImage(false);
+          setTitle('');
+          quillRef.current.root.innerHTML = '';
+          setCategory('Startup'); // default category
+      }
+      else{
+        toast.error(data.message);
+      } 
+    } catch (error) {
+        toast.error(error.message);
+    }
   }
   
   useEffect(() => {
@@ -102,9 +142,10 @@ const AddBlog = () => {
           </div>
           
           <button 
+            disabled={isAdding}
             type='submit'
             className='mt-6 w-full sm:w-auto px-8 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors'>
-            Add Blog
+            {isAdding ? 'Adding...' : 'Add Blog'}
           </button>
         </div>
       </form>
