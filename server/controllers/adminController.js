11f93ffc06d9d5afd012a7a050275c598f97e337
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-06-18 21:55:10 +0530

    Added admin controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,26 +1,53 @@
 // function that'll allow user to login for our application.
 // create login API
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import User from '../models/User.js';
 import Blog from '../models/Blog.js';
 import Comment from '../models/Comments.js';
 
-const adminLogin = async(req, res) =>{
+// Register new admin user
+const adminRegister = async(req, res) =>{
     try{
-        // from request body we'll get email & password.
-        // if email id & password matches then we'll allow user to login.
-        // also provide authentication token
-
-
-        // destructure request body
-        const {email, password} = req.body;
-
-        if(email != process.env.ADMIN_EMAIL || password != process.env.ADMIN_PASSWORD){
-            return res.json({success:false, message: 'Invalid credentials'});
+        const {name, email, password} = req.body;
+        
+        if(!name || !email || !password){
+            return res.json({success: false, message: 'All fields required'});
         }
 
-        // else if matched generate JWT token
-        const token = jwt.sign({email}, process.env.JWT_SECRET);
-        res.json({success: true, token});
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return res.json({success: false, message: 'Email already registered'});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({name, email, password: hashedPassword});
+        await newUser.save();
+
+        res.json({success: true, message: 'Registration successful. Please login.'});
+    }
+    catch(error){
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Login admin user from database
+const adminLogin = async(req, res) =>{
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.json({success: false, message: 'Email and password required'});
+        }
+
+        const user = await User.findOne({email});
+        
+        if(!user || !(await bcrypt.compare(password, user.password))){
+            return res.json({success: false, message: 'Invalid credentials'});
+        }
+
+        const token = jwt.sign({userId: user._id, email: user.email}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        res.json({success: true, token, user: {id: user._id, name: user.name, email: user.email}});
     }
     catch(error){
        res.json({success: false, message: error.message});
@@ -31,7 +58,7 @@ const adminLogin = async(req, res) =>{
 // A controller function using which user can see all blog posts
 export const getAllBlogsAdmin = async(req, res) =>{
     try{
-        const blogs = await Blog.find({}).sort({createdAt: -1}); //sort in descending order.
+        const blogs = await Blog.find({}).populate('author', 'name email').sort({createdAt: -1}); //sort in descending order.
         res.json({success:true, blogs}); 
     } 
     catch(error){
@@ -54,7 +81,7 @@ export const getAllComments = async(req, res) =>{
 export const getDashBoard = async(req, res)=>{
     try{
         // find recent blogs
-        const recentBlogs = await Blog.find({}).sort({createdAt: -1}).limit(5); 
+        const recentBlogs = await Blog.find({}).populate('author', 'name email').sort({createdAt: -1}).limit(5);
         // get 5 recent blogs // in decreasing order of there timestamps
         const blogs = await Blog.countDocuments(); // counts total number of blogs
         // count only approved comments for dashboard display
@@ -80,9 +107,8 @@ export const deleteCommentById = async (req, res)=>{
     try{
         const {id} = req.body; // access comment id from req body
         await Comment.findByIdAndDelete(id);
-
         // delete all comments associated with blog
-        await Comment.deleteMany({blog: id});
+        // await Comment.deleteMany({blog: id});
 
         res.json({success:true, message: "Comment Deleted Successfully"});
     }   
@@ -103,4 +129,5 @@ export const ApproveCommentById = async (req, res)=>{
         res.json({success:false, message: error.message});
     }
 }
+export { adminLogin, adminRegister };
 export default adminLogin;
